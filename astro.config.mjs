7cd5d849e3d799e39b1a2a7c969a834728b78d90
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2022-09-29 21:28:29 +0300

    chore: up
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
